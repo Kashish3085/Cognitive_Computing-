426230004bd78477ad53124f492da285445728e6
Kashish3085 <kkashish2_be23@thapar.edu>
2025-01-23 01:50:15 +0530

    Add files via upload

---
+++ Assignment 3/Q.3.4.py
@@ -0,0 +1,10 @@
+# Q.3.4 CSV file
+
+import pandas as pd
+d = pd.read_csv('iris.csv')
+print("\nFirst five rows of Iris dataset:\n",d.head())
+
+#Q.3.5 
+
+s= d.drop(index=4, columns=d.columns[2])
+print("\nModified Iris dataset:\n",s)
